59a61414f07252f1850edaab233214b6597da98d
Daniel Bennett <danielpsalmbennett@yahoo.com>
2023-12-31 20:00:25 -0600

    new work

--- Buildoapi/views/rareusers.py
+++ Buildoapi/views/rareusers.py
@@ -4,9 +4,12 @@ from Buildoapi.models.user import RareUser
 
 
 class RareUserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    last_name = serializers.CharField(source='user.last_name', read_only=True)
+    first_name = serializers.CharField(source='user.first_name', read_only=True)
     class Meta:
         model = RareUser
-        fields = ('id', 'bio', 'profile_image_url', 'created_on', 'active', 'user_id', 'state_name', 'county_name', 'is_contractor', "qualifications")
+        fields = ('id', 'bio', 'profile_image_url', 'created_on', 'active', 'user_id', 'state_name', 'county_name', 'is_contractor', "qualifications",'username', 'last_name','first_name')
         
 class RareUserViewSet(viewsets.ViewSet):
     pass     
