18f1239d6ddce5807bc4ff4ea02059451a10bb79
Daniel Bennett <danielpsalmbennett@yahoo.com>
2024-06-16 22:22:16 -0500

    about

--- Buildoproject/about.py
+++ Buildoproject/about.py
@@ -1,8 +1,3 @@
-## the buildo project is specifically meant for serving a purpose similar to that of homeadvisor, angie, or any other. The point of this application is to bring clients and contractors or handy man workers together. In a world where good help can be hard to find Buildo aims to bridge that gap. A review system will allow clients or onlookers to see the top rated and must trustworthy workers, and an integrated friend and messaging system will aim to help bring a new aspect of communication never before seen in an app in this field. I found myself looking for work as a handyman before, and I also have seen many people looking for workers to fix their homes or build new ones in my travels. With more To take care of and maintain with modern structures. This website serves a huge need! 
 
-## Backend notes. This backend will be changing, but it aims to be a fully dynamic database and allow many features all within itself. Thank you for taking the time to read about this awesome project!
-
-## Backend note on june 5th 2024 - adding in features on front end for this backend feature. Log and serve on this date for testing. 
- 
-
-## backend notes logging in on june 19th ready to continue the uploading of a photo feature. 
+## backend notes logging in on june 19th ready to continue the uploading of a photo feature. with our current
+## with our current iteration we have the posts working but the image uploaded is not showing up. need to show the chat gpt example of working from personal planner
