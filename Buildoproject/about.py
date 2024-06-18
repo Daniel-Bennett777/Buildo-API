4e9b167ce5cdd7429a78c6c3aef8766d7da97eab
Daniel Bennett <danielpsalmbennett@yahoo.com>
2024-06-17 21:01:28 -0500

    about

--- Buildoproject/about.py
+++ Buildoproject/about.py
@@ -1,3 +1,5 @@
 
 ## backend notes logging in on june 19th ready to continue the uploading of a photo feature. with our current
 ## with our current iteration we have the posts working but the image uploaded is not showing up. need to show the chat gpt example of working from personal planner
+
+##YES
