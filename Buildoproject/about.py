4a33ecb4af73975798ff1bc06dfb245e83dce16c
Daniel Bennett <danielpsalmbennett@yahoo.com>
2024-06-30 22:02:26 -0500

    note

--- Buildoproject/about.py
+++ Buildoproject/about.py
@@ -3,3 +3,4 @@
 ## with our current iteration we have the posts working but the image uploaded is not showing up. need to show the chat gpt example of working from personal planner
 
 ##YES
+## planning to continue my work on this tomrrow lets go! 
